5aa3b0f2a9f09a942cc1bc1a39d489a38e214417
cezarmathe <me@cezarmathe.com>
2019-12-19 13:12:36 +0200

    Add identifier loading function

--- lib/cloudflare_api.go
+++ lib/cloudflare_api.go
@@ -1,7 +1,7 @@
 package lib
 
 import (
-	"net"
+	"fmt"
 
 	"github.com/cloudflare/cloudflare-go"
 )
@@ -25,8 +25,41 @@ func NewAPI(config *Config) error {
 	return nil
 }
 
-// PerformUpdate check the zones from Cloudflare and updates the IPs if necessary
-func PerformUpdate(ip *net.IPAddr, zones []Zone) error {
+// LoadIdentifiers check the zones from Cloudflare and updates the IPs if necessary
+func LoadIdentifiers(zones *[]*Zone) error {
+
+	// check that all zones exist and get their identifiers
+	for _, zone := range *zones {
+		id, err := API.ZoneIDByName((*zone).Name)
+		if err != nil {
+			return err
+		}
+		(*zone).Identifier = &id
+
+		// check that all records exist and check their identifiers
+		for _, dnsRecord := range (*zone).Records {
+			// try to find matching records on cloudflare in this zone; only one should exist
+			recs, err := API.DNSRecords(id, cloudflare.DNSRecord{
+				Name: dnsRecord.Name,
+				Type: dnsRecord.Type,
+			})
+			if err != nil {
+				return err
+			}
+
+			// filter out bad responses
+			if len(recs) == 0 {
+				return fmt.Errorf("dns record %s does not exist on cloudflare", dnsRecord)
+			}
+			if len(recs) > 1 {
+				return fmt.Errorf("more than one dns record on cloudflare exists for this specification %s", dnsRecord)
+			}
+
+			// get the identifier
+			rec := recs[0]
+			(*dnsRecord).Identifier = &(rec.ID)
+		}
+	}
 
 	return nil
 }
