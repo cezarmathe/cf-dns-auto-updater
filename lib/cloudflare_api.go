4d8da073eaf97428ae07beb944893f7b072f330b
cezarmathe <me@cezarmathe.com>
2019-12-19 12:47:07 +0200

    Create function that performs the actual update

--- lib/cloudflare_api.go
+++ lib/cloudflare_api.go
@@ -1,6 +1,10 @@
 package lib
 
-import "github.com/cloudflare/cloudflare-go"
+import (
+	"net"
+
+	"github.com/cloudflare/cloudflare-go"
+)
 
 var (
 	// API is the cloudflare api
@@ -17,6 +21,12 @@ func NewAPI(config *Config) error {
 
 	// set the api object
 	API = api
+
+	return nil
+}
+
+// PerformUpdate check the zones from Cloudflare and updates the IPs if necessary
+func PerformUpdate(ip *net.IPAddr, zones []Zone) error {
 
 	return nil
 }
